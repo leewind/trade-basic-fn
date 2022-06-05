19e1b719af0ef7e61a1039c881fa1ddd58441554
leewind <chunmin.lcm@alibaba-inc.com>
2022-06-05 11:00:29 +0800

    [dev]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="breadt",
-    version="0.1.9",
+    version="0.1.10",
     description="Basic Module For Quant",
     # packages=find_packages(include=["base", "trader"]),
     packages=["breadt"],
