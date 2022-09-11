2bfddfcb2dc5771081c12990647cbaa6b2804f1d
Michael.Lee <leewind19841209@gmail.com>
2022-09-11 09:24:24 +0800

    [dev]

---
+++ breadt/mysql_connector.py
@@ -0,0 +1,18 @@
+import pymysql
+import configparser
+
+
+class MysqlConnector:
+    def connect(config_filename):
+        config = configparser.ConfigParser()
+        config.read(config_filename)
+
+        conn = pymysql.connect(
+            host=config["db"]["host"],
+            port=int(config["db"]["port"]),
+            user=config["db"]["user"],
+            password=config["db"]["password"],
+            database=config["db"]["database"],
+        )
+
+        return conn
