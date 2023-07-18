30fca06c98c4c8ce3afe50f6a0d5268573e0230c
mac <mac@macdeMacBook-Pro.local>
2023-07-18 13:07:19 +0800

    [dev]

--- breadt/__init__.py
+++ breadt/__init__.py
@@ -36,6 +36,7 @@ from .fmin import (
 from .strategy import Strategy
 from .trade_executor import TradeExecutor
 from .redis_connector import RedisConnector
+from .clickhouse_connect_helper import ClickHouseConnector
 
 __all__ = [
     "SharedCounter",
@@ -74,5 +75,6 @@ __all__ = [
     "check_ts_symbol",
     "RedisConnector",
     "is_debt_buy",
+    "ClickHouseConnector",
     "is_after_sys_working_time",
 ]
--- setup.py
+++ setup.py
@@ -2,9 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="breadt",
-    version="0.1.81",
+    version="0.1.82",
     description="Basic Module For Quant",
     # packages=find_packages(include=["base", "trader"]),
     packages=["breadt"],
-    install_requires=["loguru", "pika", "pymysql", "redis", "sqlalchemy", "pandas"],
+    install_requires=[
+        "loguru",
+        "pika",
+        "pymysql",
+        "redis",
+        "sqlalchemy",
+        "pandas",
+        "clickhouse_connect",
+    ],
 )
